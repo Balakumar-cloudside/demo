c11ed3d132955e6ede5f4b98ed338088f823c3ec
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2023-05-23 16:09:23 +0530

    Create inter_sales.sql

---
+++ models/example/intermediate/inter_sales.sql
@@ -0,0 +1,25 @@
+with
+
+{{config(materialized='view')}}
+
+inter_sales as(
+ SELECT
+       AVG(S.avg_unit_price),
+       {{ dbt_utils.star(from=ref('stg_sales'), except=["order_id,avg_unit_price"]) }},
+       {{ dbt_utils.star(from=ref('stg_sales_new'), except=["order_id"]) }}
+	
+FROM
+   {{ ref('stg_sales') }} AS  S
+RIGHT JOIN
+   {{ ref('stg_sales_new') }} AS  N
+ ON
+  S.order_id = N.order_id
+GROUP BY 
+       S.item_type,
+       S.sales_channel,
+       S.total_cost,
+       N.region,
+       N.country
+)
+		
+Select * from inter_sales
