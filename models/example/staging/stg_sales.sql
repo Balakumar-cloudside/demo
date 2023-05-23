88cd6d273ed05decc2a365be991b3ab6ac403e21
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2023-05-23 16:07:54 +0530

    Update stg_sales.sql

--- models/example/staging/stg_sales.sql
+++ models/example/staging/stg_sales.sql
@@ -1,1 +1,15 @@
+WITH
+sales AS (
+SELECT * FROM `cloudside-academy.demo.sales`),
 
+n AS ( 
+SELECT 
+     unit_price AS avg_unit_price,
+     item_type,
+     sales_channel,
+     total_cost,
+     order_id     
+FROM sales
+)
+
+SELECT * FROM n;
